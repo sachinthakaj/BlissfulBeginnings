2812cffa45ca719eaeee19a8df07ce5feac9d6b1
emlnvd <emil.navod@gmail.com>
2024-09-04 14:09:28 +0530

    homepage now loads

--- views/Home.php
+++ views/Home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blissful Beginnings</title>
-    <link rel="stylesheet" href="/assets/css/home.css">
+    <link rel="stylesheet" href="./assets/css/Home.css">
 </head>
 <body>
     <!-- Your HTML content here -->
