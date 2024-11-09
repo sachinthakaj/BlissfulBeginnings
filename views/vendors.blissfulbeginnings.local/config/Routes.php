37d673a1722bca3eb6916612594d4387ff6ab79a
Ishadi Magallagoda <140165616+ishadi50390@users.noreply.github.com>
2024-11-09 14:44:59 +0530

    route vendor signup,dashboard

--- views/vendors.blissfulbeginnings.local/config/Routes.php
+++ views/vendors.blissfulbeginnings.local/config/Routes.php
@@ -2,5 +2,7 @@
 
 $router->get("/", "HomeController@index");
 $router->get("/signin", "vendorAuthController@signInPage");
+$router->get("/register", "vendorAuthController@Register");
 $router->post("/register", "vendorAuthController@registerAsVendor");
 $router->get("/edit-profile/{vendorID}", "vendorController@editProfilePage");
+$router->get("/vendordashboard", "vendorAuthController@VendorDash");
