8404a600e98ada9e05d90c1ce3a7149bc6edc627
emlnvd <emil.navod@gmail.com>
2024-11-13 00:19:08 +0530

    Modal opens with the current details

--- controllers/vendorController.php
+++ controllers/vendorController.php
@@ -6,7 +6,7 @@ class vendorController
 
     public function __construct()
     {
-        $this->vendorModel=new vendor();
+        $this->vendorModel = new vendor();
     }
     public function editProfilePage()
     {
@@ -19,34 +19,34 @@ class vendorController
             'name' => "Trident Media",
             'type' => "Photographer",
             'description' => "Trident Media is a premier wedding photography studio that specializes in transforming your special day into timeless memories. With a keen eye for detail and a passion for storytelling, Trident Media's team of professional photographers ensures that every candid smile, heartfelt glance, and cherished moment is beautifully captured.",
+            'image' => "/public/assets/images/camera_1361782 1.png",
             'packages' => [
-                [
-                    'packageID'=> "123145678987654321",
+                "123145678987654321" =>[
                     'name' => 'Package 1',
                     'features' => ['Feature 1', 'Feature 2', 'Feature 3'],
                     'price' => '$99.99',
                     'state' => 'published',
                     'fixedCost' => 10000,
+                    'cameraCoverage' => 1
                 ],
-                [
-                    'packageID'=> "123145678987654322",
+                "123145678987654322" => [
                     'name' => 'Package 2',
                     'features' => ['Feature 4', 'Feature 5', 'Feature 6'],
                     'price' => '$149.99',
                     'state' => 'unpublished',
                     'fixedCost' => 10000,
-                ],[
-                    'packageID'=> "123145678987654323",
+                    'cameraCoverage' => 2
+                ],
+                "123145678987654323" => [
                     'name' => 'Package 3',
                     'features' => ['Feature 4', 'Feature 5', 'Feature 6'],
                     'price' => '$149.99',
                     'state' => 'unapproved',
                     'fixedCost' => 10000,
+                    'cameraCoverage' => 1
                 ]
-                ]
-            ];
+            ]
+        ];
         echo json_encode($data);
     }
-
-
 }
--- views/vendors.blissfulbeginnings.local/public/assets/js/editProfile.js
+++ views/vendors.blissfulbeginnings.local/public/assets/js/editProfile.js
@@ -2,10 +2,15 @@ const path = window.location.pathname;
 const pathParts = path.split('/');
 const vendorID = pathParts[pathParts.length - 1];
 
+const mainContainer = document.querySelector('.main-container');
 
-const displayPhotographerPackage = (packageDetails, divElement) => {
-    const packagesContainer = document.getElementById('packages-container');
-    packagesContainer.innerHTML = ''; // Clear any existing content
+
+const displayPhotographerPackage = (packageDetails, modalContent) => {
+    modalContent.innerHTML += `<div class="input-group">
+                                <label for="cameraCoverage">Camera Coverage</label>
+                                <input type="text" id="cameraCoverage" name="cameraCoverage" value=${packageDetails.cameraCoverage} required>
+                            </div>
+                            </form>`;
 }
 const displayDressDesignerPackage = (packageDetails, divElement) => {
     const packagesContainer = document.getElementById('packages-container');
@@ -23,9 +28,11 @@ const displayFloristPackage = (packageDetails, divElement) => {
 const vendorDisplayFunctions = {
     'Photographer': displayPhotographerPackage,
     'Dress Designer': displayDressDesignerPackage,
-    'Salon': displaySalonPackage,	
+    'Salon': displaySalonPackage,
     'Florist': displayFloristPackage
 }
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -49,17 +56,73 @@ document.addEventListener("DOMContentLoaded", () => {
             // update title and description
             document.getElementById('name').textContent = vendorData.name;
             document.getElementById('description').textContent = vendorData.description;
+            document.getElementById("profile-image").setAttribute("src", vendorData.image);
+
+            function openUpdateModal(packageID) {
+                console.log(packageID);
+                const package = vendorData.packages[packageID];
+                console.log(package);
+                const modal = document.getElementById("modal");
+                const modalContent = document.getElementById("modal-content");
+                modalContent.innerHTML = `
+                        <span class="close">&times;</span>
+                        <h2>Update Package</h2>
+                        <form id="updateForm">
+                            <div class="input-group">
+                                <label for="packageName">Package Name</label>
+                                <input type="text" id="packageName" name="packageName" value=${package.name} required>
+                            </div>
+                            <div class="input-group">
+                                <label for="feature1">Feature 1</label>
+                                <input type="text" id="feature1" name="feature1" value=${package.features[0]} required>
+                            </div>
+                            <div class="input-group">
+                                <label for="feature2">Feature 2</label>
+                                <input type="text" id="feature2" name="feature2" value=${package.features[1]} required>
+                            </div>
+                            <div class="input-group">
+                                <label for="feature3">Feature 3</label>
+                                <input type="text" id="feature3" name="feature3" value=${package.features[2]} required>
+                            </div>
+                            <div class="input-group">
+                                <label for="fixedCost">Fixed Cost</label>
+                                <input type="text" id="fixedCost" name="fixedCost" value=${package.fixedCost} required>
+                            </div>
+                            `;
+                vendorDisplayFunctions[vendorData.type](package, modalContent);
+                mainContainer.appendChild(modal);
+                modal.style.display = "block";
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function () {
+                    currentStep = 0;
+                    modal.style.display = "none";
+                }
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        currentStep = 0;
+                        modal.style.display = "none";
+                    }
+                }
+            }
 
 
             const packagesContainer = document.getElementById('packages-container');
             packagesContainer.innerHTML = ''; // Clear any existing content
 
-            vendorData.packages.forEach(package => {
+            Object.entries(vendorData.packages).forEach(([packageID, package]) => {
                 const packageDiv = document.createElement('div');
                 packageDiv.classList.add('package');
-
+                packageDiv.setAttribute("id", packageID);
                 packageDiv.innerHTML = `
-                    <div class="details">
+                <div class="details">
+                <span class="delete-icon">🗑️</span>
                         <div>${package.name}</div>
                         <div>What's Included:</div>
                         <ul>
@@ -68,9 +131,20 @@ document.addEventListener("DOMContentLoaded", () => {
                         <div class="price">${package.fixedCost}</div>
                     </div>
                 `;
-
+                packageDiv.addEventListener('click', (event) => openUpdateModal(event.currentTarget.id));
                 packagesContainer.appendChild(packageDiv);
             });
+
+            // Creating the new package card
+            const packageDiv = document.createElement('div');
+            packageDiv.classList.add('package');
+            packageDiv.innerHTML = `
+                    <div class="details add-package">
+                        <img src="/public/assets/images/Plus.png" id="plus" alt="Package Image">
+                    </div>
+                `;
+
+            packagesContainer.appendChild(packageDiv);
         })
     } catch (error) {
         console.error("Error fetching data:", error);
