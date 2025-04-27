7243711382a272478971b75cc4120e6329c7a5f1
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2025-04-27 16:13:49 +0530

    Add link to register page from the sign-in page

--- views/vendors.blissfulbeginnings.local/public/VendorSignIn.php
+++ views/vendors.blissfulbeginnings.local/public/VendorSignIn.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
     <script src="/public/assets//js/SignIn.js" defer></script>
 
 </head>
+
 <body>
     <header>
         <div class="nav-bar-logo-container">
@@ -22,15 +24,21 @@
             <!-- <img src="http://cdn.blissfulbeginnings.com/common-icons/blissful_beginnings_logo-nobg.png" alt="Blissful Beginnings Logo" class> -->
         </div>
         <div class="form-right">
-            <form class="form">
-                <h2>Vendor - Sign In</h2>
-                <label class="label" for="email">Email</label><br>
-                <input class="input" type="email" id="email" name="email" placeholder="Enter email" required><br>
-                <label class="label" for="password">Password</label><br>
-                <input class="input" type="password" id="password" name="password" placeholder="Enter password" required><span><a href="" class="forgot-password">Forgot Password >>></a></span><br><br>
-                <input type="submit" id="submit" value="Sign In">
-            </form>
+            <div class="container">
+                <form class="form">
+                    <h2>Vendor - Sign In</h2>
+                    <label class="label" for="email">Email</label><br>
+                    <input class="input" type="email" id="email" name="email" placeholder="Enter email" required><br>
+                    <label class="label" for="password">Password</label><br>
+                    <input class="input" type="password" id="password" name="password" placeholder="Enter password" required><span><a href="" class="forgot-password">Forgot Password >>></a></span><br><br>
+                    <input type="submit" id="submit" value="Sign In">
+                </form>
+                <div class="register-link">
+                    <p>Don't have an account? <a href="/register" style="color:#c1a42e;">Register here</a></p>
+                </div>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
